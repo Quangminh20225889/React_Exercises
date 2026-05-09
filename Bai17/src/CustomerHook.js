15a4382f20647b6f504e3e9e0c7bc1e85f28314a
Quangminh20225889 <trandaiquangminh2004@gmail.com>
2026-05-09 16:37:42 +0700

    fix Bai17

--- Bai17/src/CustomerHook.js
+++ Bai17/src/CustomerHook.js
@@ -2,14 +2,14 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 export default function useLocalStorage(key, initialValue){
-    const [value, setValue] = useState(() =>{
+    const [items, setItems] = useState(() =>{
         const storedValue = localStorage.getItem(key);
         if(storedValue) return JSON.parse(storedValue);
         else return initialValue;
     })
 
     useEffect(()=>{
-        localStorage.setItem(key,JSON.stringify(value))
-    },[key,value]);
-    return [value,setValue];
+        localStorage.setItem(key,JSON.stringify(items))
+    },[key,items]);
+    return [items,setItems];
 }
