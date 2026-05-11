ffaec8f27223558b63bbae1bf500c319d858b51b
Quangminh20225889 <trandaiquangminh2004@gmail.com>
2026-05-11 16:00:09 +0700

    add Bai19

---
+++ Bai19/src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Fetch failed");
+        }
+
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((err) => {
+        setError(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+
+  return (
+    <div>
+      <h1>User List</h1>
+
+      {users.map((user) => (
+        <div key={user.id}>
+          <h3>{user.name}</h3>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
